c3897935bfa6caca5b0a418a8edd34a3415cd7e1
Tom Seidel <hi@tomseidel.com>
2025-03-21 20:40:13 +0100

    Worked on the JavaScript function

--- even-odd-checker/index.js
+++ even-odd-checker/index.js
@@ -1,3 +1,10 @@
-var x = document.getElementsByClassName("number")[0].innerText;
+const number = document.getElementsByClassName("number")[0].innerHTML;
 
-console.log(x);
+console.log(number);
+
+if(number % 2 == 0) {
+    console.log("The number is even!");
+}
+else {
+    console.log("The number is odd!");
+}
