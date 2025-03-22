141ab4eb2fbb83360f4f3315de3c313656e9ffc9
Tom Seidel <hi@tomseidel.com>
2025-03-22 16:18:47 +0100

    Finished the project

--- even-odd-checker/index.js
+++ even-odd-checker/index.js
@@ -1,10 +1,27 @@
-const number = document.getElementsByClassName("number")[0].innerHTML;
+// 1. Get the elements
+var inputField = document.getElementById("numberInput");
+var button = document.getElementById("checkButton");
+var outputDiv = document.getElementById("output");
 
-console.log(number);
+// 2. Event Listener for button click
 
-if(number % 2 == 0) {
-    console.log("The number is even!");
-}
-else {
-    console.log("The number is odd!");
-}
+button.addEventListener("click", function() {
+
+    var input = parseInt(inputField.value); // number from input field
+
+    if (isNaN(input)) {
+        outputDiv.textContent = "That's not a valid number!";
+        return;
+    }
+
+    if (input % 3 == 0) {
+        outputDiv.textContent = "Fizz";
+    }
+    else if (input % 2 == 0) {
+        outputDiv.textContent = "The number is even!";
+    }
+    else {
+        outputDiv.textContent = "The number is odd!";
+    }
+
+});
