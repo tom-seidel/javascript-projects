1020e5981e789e5df4a4a2ea227694dce91bfbbf
Tom Seidel <hi@tomseidel.com>
2025-03-25 09:37:23 +0100

    Implemented user input for numbers

--- interactive-button/index.js
+++ interactive-button/index.js
@@ -1,6 +1,16 @@
+var input = document.getElementById("input");
 var button = document.getElementById("button");
 var output = document.getElementById("output");
 
 button.addEventListener("click", function() {
-    output.textContent = "Hey!";
+
+    var number = parseInt(input.value); // converts string to number
+
+    if(isNaN(number)) {
+        output.textContent = "This is not a number!";
+    }
+    else {
+        output.textContent = number;
+    }
+
 });
