e1c62e9ea9c2e9637c708105a68a5e9213e2bf3f
Tom Seidel <hi@tomseidel.com>
2025-03-27 14:51:30 +0100

    Added color code to output div

--- random-bg-color/index.js
+++ random-bg-color/index.js
@@ -1,5 +1,8 @@
 const button = document.getElementById("button");
+const output = document.getElementById("output");
 
 button.addEventListener("click", () => {
-    document.body.style.backgroundColor = '#' + (Math.random() * 0x1000000 << 0).toString(16).padStart(6, '0');
+    var color = '#' + (Math.random() * 0x1000000 << 0).toString(16).padStart(6, '0');
+    document.body.style.backgroundColor = color;
+    output.textContent = color;
 });
