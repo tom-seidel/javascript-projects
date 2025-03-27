a0c32c069465c3a843ec3705d0bf46c3efbd96be
Tom Seidel <hi@tomseidel.com>
2025-03-27 13:00:39 +0100

    Project was already done but added a second output variation

--- array-transformer/index.js
+++ array-transformer/index.js
@@ -1,4 +1,6 @@
-const numbers = [12, 22, 3, 7, 19];
 const output = document.getElementById("output");
+const numbers = [12, 22, 3, 7, 19];
 
-output.textContent = numbers.map(num => num *2).filter(num => num > 10).sort((a,b) => b - a).join(" ");
+//output.textContent = numbers.map(num => num * 2).filter(num => num > 10).sort((a,b) => b - a).join(" ");
+
+output.textContent = numbers.map(num => num * num).filter(num => num > 20).join(", ");
