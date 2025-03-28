9774725f220ebfac5fa772453c1fb3909dc0761a
Tom Seidel <hi@tomseidel.com>
2025-03-28 21:54:28 +0100

    Initial commit

---
+++ error-handler/index.js
@@ -0,0 +1,9 @@
+const numberOne = document.getElementById("numberOne");
+const numberTwo = document.getElementById("numberOne");
+const button = document.getElementById("button");
+const output = document.getElementById("output");
+
+
+button.addEventListener("click", () => {
+    output.textContent = "Test";
+})
