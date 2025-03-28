dea7862b247be5f5fa338be417aba5fd122f0cfb
Tom Seidel <hi@tomseidel.com>
2025-03-28 22:52:29 +0100

    Finished this mini project

--- error-handler/index.js
+++ error-handler/index.js
@@ -1,9 +1,23 @@
 const numberOne = document.getElementById("numberOne");
-const numberTwo = document.getElementById("numberOne");
+const numberTwo = document.getElementById("numberTwo");
 const button = document.getElementById("button");
 const output = document.getElementById("output");
 
-
 button.addEventListener("click", () => {
-    output.textContent = "Test";
-})
+    const one = parseFloat(numberOne.value); // number from number field one
+    const two = parseFloat(numberTwo.value); // number from number field two
+    
+    if(isNaN(one) || isNaN(two)) {
+        output.textContent = "Ergebnis: Bitte gütlige Zahlen eingeben!";
+        output.style.color = "red";
+        return;
+    }
+
+    if(two === 0) {
+        output.textContent = "Ergebnis: Division durch 0 ist nicht erlaubt!";
+        output.style.color = "red";        
+        return;
+    }
+
+    output.textContent = `Ergebnis: ${one / two}`;
+});
